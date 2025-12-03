806a47a5932cc95cfd8f2cd02c61936941ceab7c
igwekailota-beep <igwekailota@gmail.com>
2025-12-03 17:11:47 +0100

    project which uses struct and method

--- week-10/project_1/src/main.rs
+++ week-10/project_1/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+struct Laptop {
+    price: u32,
 }
+
+impl Laptop {
+    fn cost(&self, qty: u32) -> u32 {
+        self.price * qty
+    }
+}
+
+fn main() {
+    let laptops = [
+        Laptop { price: 650_000 }, // HP
+        Laptop { price: 755_000 }, // IBM
+        Laptop { price: 550_000 }, // Toshiba
+        Laptop { price: 850_000 }, // Dell
+    ];
+
+    
+
+
+let total: u32 = laptops.iter().map(|l| l.cost(3)).sum();
+
+       println!("Total cost: ₦{}", total);
+}
+
